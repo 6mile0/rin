acb7eecec1e04aa66201858ca819920d0aab3430
6mile <info@6mile.dev>
2022-09-28 20:15:35 +0900

    [add] 機能追加

--- index.js
+++ index.js
@@ -9,7 +9,7 @@
 
 const token = "MTAyMzQyMzI1NDI1NTM5MDc5Mw.G8jffM.oyR9ivejn3mD9ipXLaKc0ZrXe2P9S2TaNm3XmM"; // DiscordのBotのトークン(本番環境)
 const botname = "Coder"; // Botの名前
-const ver = "v1.5.0"; // 現在バージョン
+const ver = "v1.0.0"; // 現在バージョン
 
 // ========================================================
 
@@ -17,7 +17,7 @@ require('date-utils');
 var fs = require("fs");
 var exec = require('child_process').exec;
 const execSync = require('child_process').execSync;
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
 const client = new Client({
     intents: Object.values(GatewayIntentBits).reduce((a, b) => a | b)
 });
@@ -41,24 +41,16 @@ var disableLibraries = [
 
 var flag = false;
 var flagUserId = "";
+var flagUserName = "";
 
-function setTimer(time, ch) {
-    var dt = new Date();
+var delflag = false;
+var delflagUserId = "";
+
+function setTimer(name, time, ch, userId) {
     var intervalID = setInterval(() => {
-        var dtRenew = new Date();
-        var formatted = dtRenew.toFormat("HH24:MI:SS");
-
-        tt = time.split(":");
-        ms = (tt[0] * 3600 + tt[1] * 60 + tt[2] * 1) * 1000;
-        afterMs = dt.getTime() + ms;
-
-        console.log("now_ms: " + dtRenew.getTime());
-        console.log("after_ms: " + afterMs);
-        console.log("now: " + formatted);
-        client.channels.cache.get(ch).send("now_ms: " + dtRenew.getTime() + "\n after_ms: " + afterMs + "\n now: " + formatted);
-
-        if (dtRenew.getTime() === afterMs) { // ミリ秒が一致したら
-            client.channels.cache.get(ch).send("時間になりました！");
+        var dt = new Date();
+        if (dt.getTime() >= time) { // ミリ秒が一致したら
+            client.channels.cache.get(ch).send(`<@${userId}>` + "\n設定した`" + name + "`の時間になりました！");
             setTimeout(() => {
                 clearInterval(intervalID)
             })
@@ -73,46 +65,112 @@ client.on("messageCreate", async (msg) => {
     }
 
     if (flag == true && msg.author.id == flagUserId) {
-        if (msg.content === "!setTimer") {
+        if (msg.content === "!timerSet") {
             ;
         } else if (msg.content === "!cancel") {
-            msg.channel.send("> キャンセルしました");
-            flag = !flag;
-        } else if (msg.content.match(/^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/)) {
-            msg.channel.send("> `" + msg.content + "`で設定しました");
-            var setId = setTimer(msg.content, msg.channelId);
-            timerDataAry.push({
-                "id": msg.author.id,
-                "name": msg.author.username,
-                "intervalID": setId
-            });
+            msg.channel.send("> 登録をキャンセルしました");
+            flag = !flag; // フラグを戻す
+            flagUserId = ""; // 初期化
+        } else if (msg.content.match(/[^\s]+/g)) { // 空白区切りになっているか
+            var data = msg.content.split(" ");
+            console.log(data);
+            if (data.length == 2) {
+                if (data[0].length > 500) {
+                    msg.channel.send("> タイマー名は500文字以内にしてください");
+                    return;
+                } else {
+                    if (data[1].match(/^([01][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/)) {
+                        msg.channel.send("> タイマーを設定しました。" + "名称: " + data[0] + "\n時間: " + data[1]);
 
-            flag = !flag;
+                        var dt = new Date();
+                        tt = data[1].split(":");
+                        ms = (tt[0] * 3600 + tt[1] * 60 + tt[2] * 1) * 1000; // 時間をミリ秒に変換
+                        afterMs = dt.getTime() + ms; // 現在時刻にミリ秒を足す
+
+                        var setId = setTimer(data[0], afterMs, msg.channelId, msg.author.id);
+                        timerDataAry.push({
+                            "id": msg.author.id,
+                            "userName": msg.author.username,
+                            "timerName": data[0],
+                            "time": data[1],
+                            "intervalID": setId,
+                        });
+                        flag = !flag;
+                    } else {
+                        msg.channel.send("> 不正なフォーマットです。例に従い，再度入力してください。\nフォーマット例: `<タイマー名称> 00:01:00`");
+                    }
+                }
+            } else {
+                msg.channel.send("> 不正なフォーマットです。例に従い，再度入力してください。\nフォーマット例: `<タイマー名称> 00:01:00`");
+            }
         } else {
-            msg.channel.send("> 不正なフォーマットです。例にしたがって正しいフォーマットで時間を入力してください。フォーマット例: `00:01:00`");
+            msg.channel.send("> 不正なフォーマットです。例に従い，正しいフォーマットで時間を入力してください。\nフォーマット例: `<タイマー名称> 00:01:00`");
+        }
+    }
+
+    if (delflag == true && msg.author.id == delflagUserId) {
+        if (msg.content === "!timerDel") {
+            ;
+        } else if (msg.content === "!cancel") {
+            msg.channel.send("> 解除をキャンセルしました");
+            flag = !flag;
+        } else if (msg.content.match(/^[0-9]+$/)) {
+            if (msg.content == 0) {
+                msg.channel.send("> 不正な数値です。1以上の自然数で指定してください");
+                return;
+            } else if (msg.content > timerDataAry.length) {
+                msg.channel.send("> 不正な数値です。存在しない番号です。");
+                return;
+            } else {
+                var num = msg.content - 1;
+                msg.channel.send("> タイマーを解除しました。" + "名称: " + timerDataAry[num].timerName + "\n時間: " + timerDataAry[num].time);
+                clearInterval(timerDataAry[num].intervalID);
+                timerDataAry.splice(num, 1);
+                delflag = !delflag; // フラグを戻す
+                delflagUserId = ""; // 初期化
+            }
+        } else {
+            msg.channel.send("> 不正な数値です。1以上の自然数で削除するタイマーを指定してください");
         }
     }
 
 
-    if (msg.content === "!setTimer") {
+    if (msg.content === "!timerSet") {
         if (flag == true && msg.author.id == flagUserId) {
-            msg.channel.send("> すでにあなたが設定中です。同時に設定することはできません");
+            msg.channel.send("> すでにあなたが設定中です。同時に設定することはできません。");
         } else if (flag == true && msg.author.id != flagUserId) {
-            msg.channel.send("> すでに別のユーザが設定中です。同時に設定することはできません");
+            msg.channel.send(`> すでに${flagUserName}さんが設定中です。同時に設定することはできません。`);
         } else {
-            msg.channel.send("> タイマーをセットします。セットする時間を指定してください。フォーマット例: `00:01:00`");
+            msg.channel.send("> タイマーをセットします。セットする時間を指定してください。フォーマット例: `<タイマー名称> 00:01:00`");
             flag = !flag; // 対話管理フラグ
             flagUserId = msg.author.id; // 対話管理ユーザーID
+            flagUserName = msg.author.username; // 対話管理ユーザー名
         }
     }
 
-    if (msg.content === "!delTimer") {
-        if (flag == true && msg.author.id == flagUserId) {
-            msg.channel.send("> 設定されたタイマーを解除しました");
+    if (msg.content === "!timerDel") {
+        msg.channel.send("> 削除するタイマー番号を入力してください。※設定者のみ削除可能");
+        delflag = !delflag; // 対話管理削除フラグ
+        delflagUserId = msg.author.id;  // 対話管理削除ユーザーID
+    }
+
+    if (msg.content === "!timerLists") {
+        if (timerDataAry.length == 0) {
+            msg.channel.send("> 現在設定されているタイマーはありません。");
         } else {
-            msg.channel.send("> タイマーをセットします。セットする時間を指定してください。フォーマット例: `00:01:00`");
-            flag = !flag; // 対話管理フラグ
-            flagUserId = msg.author.id; // 対話管理ユーザーID
+            timerDataAry.forEach(function (element, index) {
+                const timerListsEmbed = new EmbedBuilder()
+                    .setColor(0x0099FF)
+                    .setTitle('タイマー' + (index + 1))
+                    .setDescription(`現在設定されている${index + 1} / ${timerDataAry.length}つ目のタイマーです。`)
+                    .addFields(
+                        { name: '名称', value: element["timerName"] },
+                        { name: '時間', value: element["time"] },
+                        { name: '設定者', value: element["userName"] },
+                    )
+                    .setTimestamp()
+                msg.channel.send({ embeds: [timerListsEmbed] });
+            });
         }
     }
 
