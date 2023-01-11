d9eabf7cf9be91ee8f6c001c1b26888480bdaab8
6mile <me@6mile.dev>
2023-01-11 09:31:09 +0900

    [Update] コマンド化実装中

--- index.js
+++ index.js
@@ -2,20 +2,43 @@
 // 環境設定
 // ========================================================
 
-const token = "MTAyODIwMDM3NjI3ODcyMDU3Mw.GTWcpV.ibkBATvN72XabdZfvRIjC694Eu3oX0dq2ytsvI"; // DiscordのBotのトークン(本番環境)
+// const token = "MTAyODIwMDM3NjI3ODcyMDU3Mw.GTWcpV.ibkBATvN72XabdZfvRIjC694Eu3oX0dq2ytsvI"; // DiscordのBotのトークン(本番環境)
+const token = "MTAyNzE2NDc0ODYyMzY1MDg0Ng.GFEUaU.FjTQl7mVb9k7WPLMNEw82FvvpXswo-gRx6XdEw"
 const botname = "rin"; // Botの名前
 const ver = "v1.1.3"; // 現在バージョン
 
 // ========================================================
 
 require('date-utils');
-var fs = require("fs");
+const fs = require('node:fs');
+const path = require('node:path');
 var exec = require('child_process').exec;
 const execSync = require('child_process').execSync;
-const { Client, GatewayIntentBits, EmbedBuilder } = require("discord.js");
+const { Client, GatewayIntentBits, EmbedBuilder, Collection } = require("discord.js");
 const client = new Client({
     intents: Object.values(GatewayIntentBits).reduce((a, b) => a | b)
 });
+
+// ========================================================
+// [Add] スラッシュコマンド読み込み
+// 2022/12/31
+// ========================================================
+client.commands = new Collection();
+
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const filePath = path.join(commandsPath, file);
+    const command = require(filePath);
+    if ('data' in command && 'execute' in command) {
+        client.commands.set(command.data.name, command);
+    } else {
+        console.log(`${filePath} に必要な "data" か "execute" がありません。`);
+    }
+}
+
+// ========================================================
 
 const helptxt = fs.readFileSync('help.txt', 'utf-8'); // ヘルプテキスト読み込み
 
@@ -120,6 +143,25 @@ function convertNowTime(time) { // 保存されているミリ秒を現在時刻
 function padZero(num) {
     return num.toString().padStart(2, '0');
 }
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(`${interaction.commandName} が見つかりません。`);
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({ content: 'エラーが発生しました。', ephemeral: true });
+    }
+});
+
 
 client.on("messageCreate", async (msg) => {
     if (msg.author.bot) { // bot同士の会話回避
