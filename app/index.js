8c2ff29eb9b96c75c1e9a9672fcc41b5cd4f3fa7
6mile <me@6mile.dev>
2023-02-01 12:49:02 +0900

    [Update] プログラム記述なしで実行が通ってしまう問題を修正

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const { token, botName, ver, execSpace, execUserID, execGroupID } = require('./config.json');
+const { token, botName, ver, execSpace, execUserID, execGroupID, pyChId, cChId, javaChId } = require('./config.json');
 const { exec, execSync } = require('child_process');
 const fs = require('node:fs');
 const { Client, GatewayIntentBits } = require("discord.js");
@@ -9,37 +9,30 @@ const client = new Client({
 const helptxt = fs.readFileSync('help.txt', 'utf-8'); // ヘルプテキスト読み込み
 console.log(botName + " " + ver + " を起動します"); // 起動メッセージ
 
-const errorMsg = (msg, block, err) => {
+const errorMsg = (msg, err) => {
     // エラーメッセージ生成関数 ([0]エラーコード, [1]ブロックid, [2]エラーObj)
-    if (block == 0) {
-        if (err[0] == 1) { // 実行内容が空の場合
-            msg.reply("[E1000] 実行するコードが入力されていません。詳細は`!err`を参照してください。");
-            return;
-        } else if (err[0] == 2) { // ファイル作成に失敗した場合(標準入力なしパターン)
-            msg.reply("[E1001a] 実行ファイルの作成に失敗しました。詳細は`!err`を参照してください。");
-            return;
-        } else if (err[0] == 3) { // ファイル作成に失敗した場合(標準入力ありパターン)
-            msg.reply("[E1001b] 実行ファイルまたは標準入力ファイルの作成に失敗しました。詳細は`!err`を参照してください。");
-            return;
-        } else {
-            msg.reply("[Undefined] 想定外のエラーが発生しました。詳細は`!err`を参照してください。");
-            return;
-        }
-
-    } else if (block == 1) {
-        if (err[0] == 2) {
-            msg.reply("[E1002] 実行結果の出力に失敗しました。詳細は`!err`を参照してください。");
-            return;
-        } else if (err[0] == 3) {
-            msg.reply("[E1003] 入力されたプログラムの実行に失敗しましたが、標準エラー出力が空です。再度お試しください。");
-            return;
-        } else if (err[0] == 4) {
-            msg.reply("[E1004] 構文エラーまたは既定(10s)の実行可能時間を超過したため、実行に失敗しました。エラー文を確認してください。");
-            msg.channel.send({ files: [err[2]] });
-        } else {
-            msg.reply("[Undefined] 想定外のエラーが発生しました。詳細は`!err`を参照してください。");
-            return;
-        }
+    if (err[0] == 1) { // 実行内容が空の場合
+        msg.reply("[E1000] 実行するコードが入力されていません。詳細は`!err`を参照してください。");
+        return;
+    } else if (err[0] == 2) { // ファイル作成に失敗した場合(標準入力なしパターン)
+        msg.reply("[E1001a] 実行ファイルの作成に失敗しました。詳細は`!err`を参照してください。");
+        return;
+    } else if (err[0] == 3) { // ファイル作成に失敗した場合(標準入力ありパターン)
+        msg.reply("[E1001b] 実行ファイルまたは標準入力ファイルの作成に失敗しました。詳細は`!err`を参照してください。");
+        return;
+    } else if (err[0] == 4) {
+        msg.reply("[E1002] 実行結果の出力に失敗しました。詳細は`!err`を参照してください。");
+        return;
+    } else if (err[0] == 5) {
+        msg.reply("[E1003] 入力されたプログラムの実行に失敗しましたが、標準エラー出力が空です。再度お試しください。");
+        return;
+    } else if (err[0] == 6) {
+        msg.reply("[E1004] 構文エラーまたは既定の実行可能時間を超過したため、実行に失敗しました。\nエラー文を確認してください。");
+        msg.channel.send({ files: [err[2]] });
+        return;
+    } else {
+        msg.reply("[Undefined] 想定外のエラーが発生しました。詳細は`!err`を参照してください。");
+        return;
     }
 }
 
@@ -49,21 +42,21 @@ const finishExec = (containerName) => {
         try {
             const stdout = execSync(`docker ps -q -f ancestor=${containerName}`);
             if (stdout.toString().trim() == "") { // 空文字列の場合
-                resolve([0]); // コンテナが存在しない
+                resolve(0); // コンテナが存在しない
             } else {
                 try {
                     var result = execSync(`docker kill ${stdout.toString().trim()}`);
                     if (result.toString() == stdout.toString()) {
-                        resolve([0]); // コンテナ削除成功
+                        resolve(1); // コンテナ削除成功
                     } else {
-                        reject([1, "原因不明"]); // コンテナ削除失敗
+                        reject(2); // コンテナ削除失敗
                     }
                 } catch (e) {
-                    reject([1, e]); // 例外1
+                    reject([e]); // 例外1
                 }
             }
         } catch (e) {
-            reject([1, e]); // 例外2
+            reject(e); // 例外2
         }
     });
 }
@@ -109,14 +102,14 @@ const execCommand = (containerName) => {
                         // シェル上でコマンドを実行できなかった場合のエラー処理
                         if (error !== null) {
                             if (!stderr) {
-                                reject([3, error]); // E1003
+                                reject([5, error]); // E1003
                             } else {
                                 try {
                                     fs.writeFile(`/Containers/${containerName}/run/output.txt`, stderr, er => { if (er) throw er });
                                 } catch {
-                                    reject([2, er]); // E1002
+                                    reject([4, er]); // E1002
                                 }
-                                reject([4, stderr, `/Containers/${containerName}/run/output.txt`]); // E1004
+                                reject([6, stderr, `/Containers/${containerName}/run/output.txt`]); // E1004
                             }
                         }
 
@@ -124,7 +117,7 @@ const execCommand = (containerName) => {
                             // 標準出力をファイルに出力
                             fs.writeFile(`/Containers/${containerName}/run/output.txt`, stdout, er => { if (er) throw er });
                         } catch {
-                            reject([2, er]); // E1002
+                            reject([4, er]); // E1002
                         }
 
                         if (stdout.length == 0) {
@@ -140,14 +133,14 @@ const execCommand = (containerName) => {
                         // シェル上でコマンドを実行できなかった場合のエラー処理
                         if (error !== null) {
                             if (!stderr) {
-                                reject([3, error]); // E1003
+                                reject([5, error]); // E1003
                             } else {
                                 try {
                                     fs.writeFile(`/Containers/${containerName}/run/output.txt`, stderr, er => { if (er) throw er });
                                 } catch {
-                                    reject([2, er]); // E1002
+                                    reject([4, er]); // E1002
                                 }
-                                reject([4, stderr, `/Containers/${containerName}/run/output.txt`]); // E1004
+                                reject([6, stderr, `/Containers/${containerName}/run/output.txt`]); // E1004
                             }
                         }
 
@@ -155,7 +148,7 @@ const execCommand = (containerName) => {
                             // 標準出力をファイルに出力
                             fs.writeFile(`/Containers/${containerName}/run/output.txt`, stdout, er => { if (er) throw er });
                         } catch {
-                            reject([2, er]); // E1002
+                            reject([4, er]); // E1002
                         }
 
                         if (stdout.length == 0) {
@@ -168,7 +161,7 @@ const execCommand = (containerName) => {
             }
         } catch (e) {
             console.log(e);
-            reject([1, e]); // 未定義のエラー
+            reject([99, e]); // 未定義のエラー
         }
     })
 };
@@ -193,38 +186,29 @@ client.on("messageCreate", async (msg) => {
         msg.channel.send("障害対応・緊急の場合は`ろくまいる#5659`までご連絡ください。");
     }
 
-    if (msg.content.substring(0, 3) == "!py") { // !pで始まるメッセージのみ反応
+    if ((msg.channel.id == pyChId) && (msg.content.substring(0, 4) == "!pyt")) { // !pで始まるメッセージのみ反応
         var message = msg.content.split('```'); // メッセージを```で分割 (Ex:[ '!py', 'print("helloWorld")', '', 'こんにちは', '' ])
         console.log(message.length);
         console.log(message);
 
-        msg.channel.send("[Info] プログラムの実行を開始します");
+        msg.channel.send("[Info] プログラム(Python)の実行を開始します");
 
-        // 実行ファイル作成
-        await makeFiles(message, "main.py", "c1").then((res) => {
+        await Promise.all([makeFiles(message, "main.py", "c1"), execCommand("c1")]).then((res) => { // ファイル作成, 実行, コンテナ削除
             // res[0] = 状態コード, res[1] = エラー
-            if (res[0] != 0) {
-                msg.reply("[Serious] 例外が発生しました。開発者に報告してください。");
-            } else {
+            if (res[0][0] == 0) {
                 console.log("OK: ファイル作成完了");
-                console.log(`実行内容：\n${res[1]}`);
+                console.log(`実行内容：\n${res[0][1]}`);
+            } else {
+                msg.reply("[Serious] 例外が発生しました。開発者に報告してください。");
             }
-        }).catch((e) => {
-            console.log(e);
-            // 定形エラー処理
-            // e[0] = エラーコード, e[1] = エラー内容
-            errorMsg(msg, 0, e);
-        });
 
-        // 実行
-        await execCommand("c1").then((res) => {
             // res[0] = 状態コード, res[1] = 標準出力, res[2] = 出力ファイルパス
-            if (res[0] == 0) {
+            if (res[1][0] == 0) {
                 console.log("OK: 実行完了");
-                console.log(`実行結果：\n${res[1]}`);
+                console.log(`実行結果：\n${res[1][1]}`);
                 msg.reply("[Done] 実行結果は以下です。ご確認ください。"); // 実行結果を返信
-                msg.channel.send({ files: [res[2]] }) // 実行結果を添付
-            } else if (res[0] == 1) {
+                msg.channel.send({ files: [res[1][2]] }) // 実行結果を添付
+            } else if (res[1][0] == 1) {
                 msg.reply("[Done] 標準出力は空ですが、入力されたプログラムは正常に実行されました。");
             } else {
                 msg.reply("[Serious] 実行中に例外が発生しました。開発者に報告してください。");
@@ -233,64 +217,63 @@ client.on("messageCreate", async (msg) => {
             console.log(e);
             // 定形エラー処理
             // e[0] = エラーコード, e[1] = エラー内容
-            errorMsg(msg, 1, e);
+            errorMsg(msg, e);
         });
 
         await finishExec("rin/c1").then((res) => { // res[0] = 状態コード, res[1] = エラー
-            console.log(res);
-            console.log("OK: 終了処理完了");
+            if (res == 1) {
+                console.log("OK: 残留コンテナ削除成功");
+            } else if (res == 2) {
+                msg.reply("[Serious] 終了処理で想定外のエラーが発生しました。開発者に報告してください。");
+            } else { console.log("OK: 残留コンテナなし"); }
         }).catch((e) => {
             console.log(e);
             msg.reply("[Serious] 終了処理で重大なエラーが発生しました。開発者に報告してください。");
         });
     }
 
-    if (msg.content.substring(0, 2) == "!c") { // !cで始まるメッセージのみ反応
+    if ((msg.channel.id == cChId) && (msg.content.substring(0, 2) == "!c")) { // !cで始まるメッセージのみ反応
         var message = msg.content.split('```'); // メッセージを```で分割 (Ex:[ '!py', 'print("helloWorld")', '', 'こんにちは', '' ])
         console.log(message.length);
         console.log(message);
 
-        msg.channel.send("[Info] プログラムの実行を開始します");
+        msg.channel.send("[Info] プログラム(C言語)の実行を開始します");
 
-        // 実行ファイル作成
-        await makeFiles(message, "main.c", "c2").then((res) => {
+        await Promise.all([makeFiles(message, "main.c", "c2"), execCommand("c2")]).then((res) => {
             // res[0] = 状態コード, res[1] = エラー
-            if (res[0] != 0) {
+            if (res[0][0] != 0) {
                 msg.reply("[Serious] 例外が発生しました。開発者に報告してください。");
             } else {
                 console.log("OK: ファイル作成完了");
-                console.log(`実行内容：\n${res[1]}`);
+                console.log(`実行内容：\n${res[0][1]}`);
             }
-        }).catch((e) => {
-            console.log(e);
-            // 定形エラー処理
-            // e[0] = エラーコード, e[1] = エラー内容
-            errorMsg(msg, 0, e);
-        });
 
-        // 実行
-        await execCommand("c2").then((res) => {
             // res[0] = 状態コード, res[1] = 標準出力, res[2] = 出力ファイルパス
-            if (res[0] == 0) {
+            if (res[1][0] == 0) {
                 console.log("OK: 実行完了");
-                console.log(`実行結果：\n${res[1]}`);
+                console.log(`実行結果：\n${res[1][1]}`);
                 msg.reply("[Done] 実行結果は以下です。ご確認ください。"); // 実行結果を返信
-                msg.channel.send({ files: [res[2]] }) // 実行結果を添付
-            } else if (res[0] == 1) {
+                msg.channel.send({ files: [res[1][2]] }) // 実行結果を添付
+            } else if (res[1][0] == 1) {
                 msg.reply("[Done] 標準出力は空ですが、入力されたプログラムは正常に実行されました。");
             } else {
                 msg.reply("[Serious] 実行中に例外が発生しました。開発者に報告してください。");
             }
+            console.log(res[2]);
+            console.log("OK: 終了処理完了");
         }).catch((e) => {
             console.log(e);
             // 定形エラー処理
             // e[0] = エラーコード, e[1] = エラー内容
-            errorMsg(msg, 1, e);
+            errorMsg(msg, e);
         });
 
-        await finishExec("rin/c2").then((res) => { // res[0] = 状態コード, res[1] = エラー
-            console.log(res);
-            console.log("OK: 終了処理完了");
+        await finishExec("rin/c1").then((res) => { // res[0] = 状態コード, res[1] = エラー
+            if (res == 1) {
+                console.log("OK: 残留コンテナ削除成功");
+            } else if (res == 2) {
+                msg.reply("[Serious] 終了処理で想定外のエラーが発生しました。開発者に報告してください。");
+            } else { console.log("OK: 残留コンテナなし"); }
         }).catch((e) => {
             console.log(e);
             msg.reply("[Serious] 終了処理で重大なエラーが発生しました。開発者に報告してください。");
