65739df301d309c3e013e1ac9932233fb621d12e
6mile <me@6mile.dev>
2023-02-22 14:16:01 +0900

    [Add] なでしこを追加

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-const { token, botName, ver, execSpace, execUserID, execGroupID, pyChId, cChId, javaChId } = require('./config.json');
+const { token, botName, ver, execSpace, execUserID, execGroupID, pyChId, cChId, javaChId, nodeChId, nako3ChId } = require('./config.json');
 const { exec, execSync } = require('child_process');
 const fs = require('node:fs');
 const { Client, GatewayIntentBits } = require("discord.js");
@@ -158,6 +158,37 @@ const execCommand = (containerName) => {
                             resolve([0, stdout, `/Containers/${containerName}/run/output.txt`]);  // 実行結果返却
                         }
                     });
+            } else if (containerName == "c5") { // なでしこの場合
+                exec(`docker run --name ${containerName} --rm -u ${execUserID}:${execGroupID} -v /etc/group:/etc/group:ro -v /etc/passwd:/etc/passwd:ro -v ${execSpace}/Containers/${containerName}/run:/run:ro rin/${containerName}`, { timeout: 10000 },
+                    function (error, stdout, stderr) {
+                        // シェル上でコマンドを実行できなかった場合のエラー処理
+                        if (error !== null) {
+                            if (!stderr) {
+                                reject([5, error]); // E1003
+                            } else {
+                                try {
+                                    fs.writeFile(`/Containers/${containerName}/run/output.txt`, stderr, er => { if (er) throw er });
+                                } catch {
+                                    reject([4, er]); // E1002
+                                }
+                                reject([6, stderr, `/Containers/${containerName}/run/output.txt`]); // E1004
+                            }
+                        }
+
+                        try {
+                            // 標準出力をファイルに出力
+                            fs.writeFile(`/Containers/${containerName}/run/output.txt`, stdout, er => { if (er) throw er });
+                        } catch {
+                            reject([4, er]); // E1002
+                        }
+
+                        if (stdout.length == 0) {
+                            // 標準出力が空の場合
+                            resolve([0, "", `/Containers/${containerName}/run/output.txt`]); // 実行結果返却
+                        } else {
+                            resolve([0, stdout, `/Containers/${containerName}/run/output.txt`]);  // 実行結果返却
+                        }
+                    });
             }
         } catch (e) {
             console.log(e);
@@ -186,7 +217,7 @@ client.on("messageCreate", async (msg) => {
         msg.channel.send("障害対応・緊急の場合は`ろくまいる#5659`までご連絡ください。");
     }
 
-    if ((msg.channel.id == pyChId) && (msg.content.substring(0, 4) == "!pyt")) { // !pで始まるメッセージのみ反応
+    if ((msg.channel.id == pyChId) && (msg.content.substring(0, 3) == "!py")) { // !pで始まるメッセージのみ反応
         var message = msg.content.split('```'); // メッセージを```で分割 (Ex:[ '!py', 'print("helloWorld")', '', 'こんにちは', '' ])
         console.log(message.length);
         console.log(message);
@@ -259,8 +290,6 @@ client.on("messageCreate", async (msg) => {
             } else {
                 msg.reply("[Serious] 実行中に例外が発生しました。開発者に報告してください。");
             }
-            console.log(res[2]);
-            console.log("OK: 終了処理完了");
         }).catch((e) => {
             console.log(e);
             // 定形エラー処理
@@ -269,6 +298,52 @@ client.on("messageCreate", async (msg) => {
         });
 
         await finishExec("rin/c1").then((res) => { // res[0] = 状態コード, res[1] = エラー
+            if (res == 1) {
+                console.log("OK: 残留コンテナ削除成功");
+            } else if (res == 2) {
+                msg.reply("[Serious] 終了処理で想定外のエラーが発生しました。開発者に報告してください。");
+            } else { console.log("OK: 残留コンテナなし"); }
+        }).catch((e) => {
+            console.log(e);
+            msg.reply("[Serious] 終了処理で重大なエラーが発生しました。開発者に報告してください。");
+        });
+    }
+
+    if ((msg.channel.id == nako3ChId) && (msg.content.substring(0, 6) == "!nako3")) { // !nakoで始まるメッセージのみ反応
+        var message = msg.content.split('```'); // メッセージを```で分割 (Ex:[ '!py', 'print("helloWorld")', '', 'こんにちは', '' ])
+        console.log(message.length);
+        console.log(message);
+
+        msg.channel.send("[Info] プログラム(なでしこv3)の実行を開始します");
+
+        await Promise.all([makeFiles(message, "main.nako3", "c5"), execCommand("c5")]).then((res) => {
+            // res[0] = 状態コード, res[1] = エラー
+            if (res[0][0] != 0) {
+                msg.reply("[Serious] 例外が発生しました。開発者に報告してください。");
+            } else {
+                console.log("OK: ファイル作成完了");
+                console.log(`実行内容：\n${res[0][1]}`);
+            }
+
+            // res[0] = 状態コード, res[1] = 標準出力, res[2] = 出力ファイルパス
+            if (res[1][0] == 0) {
+                console.log("OK: 実行完了");
+                console.log(`実行結果：\n${res[1][1]}`);
+                msg.reply("[Done] 実行結果は以下です。ご確認ください。"); // 実行結果を返信
+                msg.channel.send({ files: [res[1][2]] }) // 実行結果を添付
+            } else if (res[1][0] == 1) {
+                msg.reply("[Done] 標準出力は空ですが、入力されたプログラムは正常に実行されました。");
+            } else {
+                msg.reply("[Serious] 実行中に例外が発生しました。開発者に報告してください。");
+            }
+        }).catch((e) => {
+            console.log(e);
+            // 定形エラー処理
+            // e[0] = エラーコード, e[1] = エラー内容
+            errorMsg(msg, e);
+        });
+
+        await finishExec("rin/c5").then((res) => { // res[0] = 状態コード, res[1] = エラー
             if (res == 1) {
                 console.log("OK: 残留コンテナ削除成功");
             } else if (res == 2) {
